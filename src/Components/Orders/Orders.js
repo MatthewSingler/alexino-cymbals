eeafb8a7147d8462bbc46745ebf9dbc2be386f1b
Matthew Singler <matthew.singler@gmail.com>
2021-09-15 16:08:52 -0500

    added delete button and it now deletes an order when clicked.

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -1,35 +1,64 @@
-import userEvent from "@testing-library/user-event"
+
 import React, { useEffect, useState } from "react"
-import { useHistory } from "react-router"
-import { Customers } from "../Users/Users"
+import { useHistory, useParams } from "react-router"
 
 export const Orders = () => {
     const [orders, setOrders] = useState([])
+    const orderId = useParams()
     const currentUser = parseInt(localStorage.getItem("alexino_user"))
 
     const history = useHistory()
-
+    const allUserOrders = () => {
+        fetch(`http://localhost:8088/orders?_expand=cymbal&userId=${currentUser}`)
+            .then(response => response.json())
+            .then((ordersArray) => {
+                setOrders(ordersArray)
+            })
+    }
 
     useEffect(
         () => {
-            fetch(`http://localhost:8088/orders?_expand=cymbal&userId=${currentUser}`)
-                .then(response => response.json())
-                .then((ordersArray) => {
-                    setOrders(ordersArray)
-                })
+            allUserOrders()
         },
-        []
+        [orderId]
     )
+    const deleteCymbal = (id) => {
+        
+        fetch(`http://localhost:8088/orders/${id}`, {
+            method: "DELETE"
+        })
+            .then(allUserOrders())
+    }
 
     return (
         <>
+            <div>
+
+                <h3>Current Order Includes {`${orders.length}`} cymbals</h3>
             {
                 orders.map(
                     (orderObj) => {
-                        return <div key={`order--${orderObj.id}`}>{orderObj.cymbal?.name}, {orderObj.cymbal?.size}, ${orderObj.cymbal.price}</div>
+                        return (
+                         <>
+                            <div key={`order--${orderObj.id}`}>{orderObj.cymbal?.name}, {orderObj.cymbal?.size}, ${orderObj.cymbal.price}</div>
+                                <div>
+                                <button onClick={() => deleteCymbal(orderObj.id)}>Delete</button>
+                                </div>
+                            </>
+                        )   
                     }
-                )
-            }
+                    )
+                }
+            </div>
         </>
     )
 }
+
+/*const deletedObj = {
+    orderId: parseInt(orderId)
+}
+const fetchOptions = {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(deletedObj)
+}*/
