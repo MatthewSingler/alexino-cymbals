90e1823720fd186568ad0bada59b677d70015c29
Matthew Singler <matthew.singler@gmail.com>
2021-09-20 15:18:32 -0500

    styling for all modules set.

--- src/Components/Cymbals/SingleCymbal.js
+++ src/Components/Cymbals/SingleCymbal.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react"
 import { useParams } from "react-router"
 import { useHistory } from "react-router"
-import InstagramEmbed from 'react-instagram-embed';
+import "./Cymbals.css"
 
 export const EachCymbal = () => {
     const [ cymbal, selectCymbal ] = useState({})
@@ -38,30 +38,45 @@ export const EachCymbal = () => {
 
     return (
         <>
-            <h2>Specs</h2>
-            <section className="cymbal">
-                <h3 className="cymbal__name">{cymbal.name}</h3>
+            <div className="cymbal__info">
+                
+                <section className="cymbal">
+                    <h3 className="cymbal__name">{cymbal.name}</h3>
                 
                 
-                <div className="cymbal__video">
-                    <iframe src={cymbal.url}
+                    <div className="cymbal__video" style={{
+                        position: "relative",
+                        paddingBottom: "56.25%",
+                        paddingTop: 25,
+                        height: 0
+                    }}>
+                        <iframe style={{
+                            position: "absolute",
+                            top: 0,
+                            bottom: 0,
+                            left: 0,
+                            width: "100%",
+                            height: "100%"
+                        }}
+                            src={cymbal.url}
                         title="YouTube video player"
                         frameborder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowFullScreen>
-                    </iframe>
-                    
-
-                </div>
-
-                <div className="cymbal__cymbalTypeId">{cymbal.cymbalType?.type}</div>
-                <div className="cymbal__size">{cymbal.size}</div>
-                <div className="cymbal__weight">{cymbal.weight}</div>
-                <div className="cymbal__price">${cymbal.price} dollars</div>
+                        </iframe>
+                    </div>
+                <h2 className="specifics">Specs</h2>
+                    <div className="cymbal__specs">
+                        <div className="cymbal__cymbalTypeId">Type: {cymbal.cymbalType?.type}</div>
+                        <div className="cymbal__size">{cymbal.size}</div>
+                        <div className="cymbal__weight">{cymbal.weight}</div>
+                        <div className="cymbal__price">${cymbal.price} dollars</div>
+                    </div>
                 <div>
-                    <button onClick={() => purchaseCymbal()}>Purchase</button>
+                    <button className="purchase" onClick={() => purchaseCymbal()}>Purchase</button>
                 </div>
-            </section>
+                </section>
+            </div>
             <button className="return" onClick={() => history.push("/cymbals")}>Check Out More Cymbals</button>
             
         </>
