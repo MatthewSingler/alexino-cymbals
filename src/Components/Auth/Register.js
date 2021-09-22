ada7c807e7f8b650da78de258f2f3cb68c875813
Matthew Singler <matthew.singler@gmail.com>
2021-09-22 09:22:13 -0500

    fixed route to cymbals main page upon register

--- src/Components/Auth/Register.js
+++ src/Components/Auth/Register.js
@@ -29,7 +29,7 @@ export const Register = (props) => {
                         .then(createdUser => {
                             if (createdUser.hasOwnProperty("id")) {
                                 localStorage.setItem("alexino_user", createdUser.id)
-                                history.push("/")
+                                history.push("/cymbals")
                             }
                         })
                 }
